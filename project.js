021f267b46ccf14d3e9116ee4f58acc3d0a69f98
Lewis <lewis.garnett96@yahoo.com>
2023-12-13 20:19:19 -0500

    project game😃

---
+++ project.js
@@ -0,0 +1,9 @@
+//1. Despot some money
+//2. Collect a bet amount
+//3.Determine number of lines to bet on
+//4.Spin the slot machine
+//5Check if user won
+//6. Give the user the money
+//7 play again
+
+const deposit = () =>
