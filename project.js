d7acb2fc75c35a952bfe4bc3931d6f61c5b35469
Lewis <lewis.garnett96@yahoo.com>
2023-12-14 22:30:26 -0500

    added while loop

--- project.js
+++ project.js
@@ -6,4 +6,19 @@
 //6. Give the user the money
 //7 play again
 
-const deposit = () =>
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while (true) {
+         const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+        console.log("Invalid deposit amount, try again.");
+    } else {
+        return numberDepositAmount;
+    } 
+    }
+  
+};
+const depositAmount = deposit();
+console.log(depositAmount);
