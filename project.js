a2d29a34f9b76261b7ca6c7e385397556beaa1d6
Lewis <lewis.garnett96@yahoo.com>
2023-12-15 23:33:25 -0500

    added getBet and numberoflined

--- project.js
+++ project.js
@@ -9,16 +9,38 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-    while (true) {
-         const depositAmount = prompt("Enter a deposit amount: ");
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
     const numberDepositAmount = parseFloat(depositAmount);
+
     if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-        console.log("Invalid deposit amount, try again.");
+      console.log("Invalid deposit amount, try again.");
     } else {
-        return numberDepositAmount;
-    } 
+      return numberDepositAmount;
     }
-  
+  }
 };
-const depositAmount = deposit();
-console.log(depositAmount);
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+const getBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Invalid bet, try again.");
+    } else {
+      return numberBet;
+    }
+  }
+};
